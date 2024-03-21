747112faa0e23fef396b11d63eb2018609c77844
Szymon Furyk <szymon.furyk@hotmail.com>
2024-03-21 12:01:42 +0100

    added drag and drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DragAndDropDirective } from './directives/drag-and-drop.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    DragAndDropDirective
   ],
   imports: [
     BrowserModule,
